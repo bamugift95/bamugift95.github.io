cbae1eb86523624726029b5cb0307edc0b53896f
bamugift95 <61422098+bamugift95@users.noreply.github.com>
2021-12-18 19:08:39 +0100

    Delete interface.js

--- assets/js/interface.js
+++
@@ -1,6 +0,0 @@
-// var minersAddr = '0xfc4f0eD0972e3f635c231f8e9DB10b47AF10b74A';
-// var tokenAddr = '0x78867BbEeF44f2326bF8DDd1941a4439382EF2A7';
-
-var minersAddr = 'h';
-var tokenAddr = 'hi';
-var minersAbi = 
